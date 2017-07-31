ab8125d0015c3c72cf172451537465e8383324f9
wsiewert <wsiewert94@gmail.com>
2017-07-31 08:26:48 -0500

    added more tests

--- CustomList/CustomList/Program.cs
+++ CustomList/CustomList/Program.cs
@@ -10,7 +10,7 @@ namespace CustomListProject
     {
         static void Main(string[] args)
         {
-            CustomList<int> list = new CustomList<int>() { 10, 5, 9, 2 };
+            CustomList<int> list = new CustomList<int>() { 11, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 9 };
             list.Sort();
             string printThis = list.ToString();
             Console.WriteLine(printThis);
--- CustomList/CustomListTest/SortTest.cs
+++ CustomList/CustomListTest/SortTest.cs
@@ -62,5 +62,87 @@ namespace CustomListTest
             //Assert
             Assert.AreEqual(expectedResult, index);
         }
+
+        [TestMethod]
+        public void Sort_IncreasingIntList_ReturnIndexZero()
+        {
+            //Arrange
+            CustomList<int> list1 = new CustomList<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int testIndex = 0;
+            int expectedResult = 1;
+            //Act
+            list1.Sort();
+            int index = list1[testIndex];
+            //Assert
+            Assert.AreEqual(expectedResult, index);
+        }
+
+        [TestMethod]
+        public void Sort_IncreasingIntList_ReturnIndexNine()
+        {
+            //Arrange
+            CustomList<int> list1 = new CustomList<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int testIndex = 9;
+            int expectedResult = 10;
+            //Act
+            list1.Sort();
+            int index = list1[testIndex];
+            //Assert
+            Assert.AreEqual(expectedResult, index);
+        }
+
+        [TestMethod]
+        public void Sort_DecreasingIntList_ReturnIndexZero()
+        {
+            //Arrange
+            CustomList<int> list1 = new CustomList<int>() { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            int testIndex = 0;
+            int expectedResult = 1;
+            //Act
+            list1.Sort();
+            int index = list1[testIndex];
+            //Assert
+            Assert.AreEqual(expectedResult, index);
+        }
+
+        [TestMethod]
+        public void Sort_DecreasingIntList_ReturnIndexNine()
+        {
+            //Arrange
+            CustomList<int> list1 = new CustomList<int>() { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            int testIndex = 9;
+            int expectedResult = 10;
+            //Act
+            list1.Sort();
+            int index = list1[testIndex];
+            //Assert
+            Assert.AreEqual(expectedResult, index);
+        }
+
+        [TestMethod]
+        public void Sort_EmptyList_ReturnCount()
+        {
+            //Arrange
+            CustomList<int> list1 = new CustomList<int>();
+            int expectedResult = 0;
+            //Act
+            list1.Sort();
+            int count = list1.Count;
+            //Assert
+            Assert.AreEqual(expectedResult, count);
+        }
+
+        [TestMethod]
+        public void Sort_StringList_ReturnString()
+        {
+            //Arrange
+            CustomList<string> list1 = new CustomList<string>() { "World", "Hello" };
+            string expectedResult = "'Hello', 'World'";
+            //Act
+            list1.Sort();
+            string result = list1.ToString();
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
